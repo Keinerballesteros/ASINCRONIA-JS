d9e9ee3858fbe0d65f0eb56b6e73019fd88ec8e2
SCIS3-PC07\SCIS3-07 <colombia@hotmail.com>
2024-06-05 11:17:40 -0500

    load

---
+++ Asincronia.js
@@ -0,0 +1,22 @@
+/*
+function sayHello(n){
+    console.log("saludo "+n)
+}
+function sayHellodelete() {
+    setTimeout(function (){
+        console.log("Saludo demorado")
+    }, 500) 
+    
+}
+
+sayHello(1)
+sayHellodelete()
+sayHello(2)
+*/
+
+function sumar(acumulado,valor){
+    return acumulado + valor
+}
+
+let acumulado = 0;
+acumulado = sumar(acumulado, 2)
